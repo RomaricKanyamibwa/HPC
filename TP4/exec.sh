5b70dd85c38c6276758e7913f1f508f37c7426dd
Romaric Kanyamibwa <romaric.kanyamibwa@gmail.com>
2018-03-04 17:08:24 +0100

    exec script

---
+++ TP4/exec.sh
@@ -0,0 +1,44 @@
+echo "filtre:0 nbiter:1,10,100"
+./convol_seq Images/Sukhothai_4080x6132.ras 0 1
+./convol_seq Images/Sukhothai_4080x6132.ras 0 10
+./convol_seq Images/Sukhothai_4080x6132.ras 0 100
+
+echo "filtre:1 nbiter:1,10,100"
+./convol_seq Images/Sukhothai_4080x6132.ras 1 1
+./convol_seq Images/Sukhothai_4080x6132.ras 1 10
+./convol_seq Images/Sukhothai_4080x6132.ras 1 100
+
+echo "filtre:2 nbiter:1,10,100"
+./convol_seq Images/Sukhothai_4080x6132.ras 2 1
+./convol_seq Images/Sukhothai_4080x6132.ras 2 10
+./convol_seq Images/Sukhothai_4080x6132.ras 2 100
+
+echo "filtre:3 nbiter:1,10,100"
+./convol_seq Images/Sukhothai_4080x6132.ras 3 1
+./convol_seq Images/Sukhothai_4080x6132.ras 3 10
+./convol_seq Images/Sukhothai_4080x6132.ras 3 100
+
+echo "filtre:4 nbiter:1,10,100"
+./convol_seq Images/Sukhothai_4080x6132.ras 4 1
+./convol_seq Images/Sukhothai_4080x6132.ras 4 10
+./convol_seq Images/Sukhothai_4080x6132.ras 4 100
+
+echo "NP:2 filtre:0 nbiter:1,10,100"
+mpirun -n 2 ./convol Images/Sukhothai_4080x6132.ras 0 1
+mpirun -n 2 ./convol Images/Sukhothai_4080x6132.ras 0 10
+mpirun -n 2 ./convol Images/Sukhothai_4080x6132.ras 0 100
+
+echo "NP:4 filtre:0 nbiter:1,10,100"
+mpirun -n 4 ./convol Images/Sukhothai_4080x6132.ras 0 1
+mpirun -n 4 ./convol Images/Sukhothai_4080x6132.ras 0 10
+mpirun -n 4 ./convol Images/Sukhothai_4080x6132.ras 0 100
+
+echo "NP:8 filtre:0 nbiter:1,10,100"
+mpirun -n 8 ./convol Images/Sukhothai_4080x6132.ras 0 1
+mpirun -n 8 ./convol Images/Sukhothai_4080x6132.ras 0 10
+mpirun -n 8 ./convol Images/Sukhothai_4080x6132.ras 0 100
+
+echo "NP:16 filtre:0 nbiter:1,10,100"
+mpirun -n 16 ./convol Images/Sukhothai_4080x6132.ras 0 1
+mpirun -n 16 ./convol Images/Sukhothai_4080x6132.ras 0 10
+mpirun -n 16 ./convol Images/Sukhothai_4080x6132.ras 0 100
